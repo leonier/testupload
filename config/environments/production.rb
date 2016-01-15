e6499b27265308ea2e23b9d39854b87c4ff21f5a
leonier <utaujinkela@gmail.com>
2016-01-15 06:20:12 +0000

    Add jquery-ui assets for Production mode.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,8 +46,10 @@ Testupload::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  # config.assets.precompile += %w( search.js )
-
+   config.assets.precompile += %w( jquery-ui.js )
+   config.assets.precompile += %w( jquery.timepicker.js )
+   config.assets.precompile += %w( jquery-ui.css )
+   config.assets.precompile += %w( jquery.timepicker.css )
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
 
